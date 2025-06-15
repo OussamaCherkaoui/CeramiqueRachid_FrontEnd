71418c72f8bb9516afa89d84b58fc9a77e178fe6
OussamaCherkaoui <OussamaCherkaoui2019@gmail.com>
2025-06-15 17:57:29 +0200

    ajouté des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {AcceuilComponent} from "./components/acceuil/acceuil.component";
+import {ProduitsComponent} from "./components/produits/produits.component";
+import {PromotionsComponent} from "./components/promotions/promotions.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {AuthentificationComponent} from "./components/authentification/authentification.component";
+import {AdministrationComponent} from "./components/administration/administration.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AcceuilComponent},
+  { path: 'nosProduits', component: ProduitsComponent},
+  { path: 'nosPromotions', component: PromotionsComponent},
+  { path: 'contactez-nous', component: ContactComponent},
+  { path: 'seConnecter', component: AuthentificationComponent},
+  { path: 'administration', component: AdministrationComponent},
+];
