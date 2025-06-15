dadfc723c2778abed2cd49dd84c75f587250dc3a
OussamaCherkaoui <OussamaCherkaoui2019@gmail.com>
2025-06-15 17:57:11 +0200

    des modifications sur la configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
 import {intercepteurInterceptor} from "./services/intercepteur.interceptor";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),provideHttpClient(withInterceptors([intercepteurInterceptor]))]
+  providers: [provideRouter(routes),provideHttpClient(withInterceptors([intercepteurInterceptor])), provideAnimationsAsync()]
 };
